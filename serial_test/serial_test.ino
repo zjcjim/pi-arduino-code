7c4058713f391ec76ec59980138ccef567ba42e1
zjc <zjcjim@outlook.com>
2024-07-13 23:28:01 +0800

    perfect run serial communication

--- serial_test/serial_test.ino
+++ serial_test/serial_test.ino
@@ -6,7 +6,8 @@ unsigned long startTime;
 unsigned long endTime;
 unsigned long elapsedTime;
 
-String data;
+String data = "";
+bool stringComplete = false;
 
 void setup() {
     // put your setup code here, to run once:
@@ -15,46 +16,57 @@ void setup() {
 
 void loop() {
     // put your main code here, to run repeatedly:
-    if (Serial.available()) {
+    
         // 读取一行数据，直到换行符'\n'
         startTime = millis();
 
-        while (Serial.available()) {
-            char c = Serial.read();
-            if (c == '\n') {
-                break;
-            }
-            data += c;
-        }
+        if (stringComplete) {
+          //Serial.println(data);
+          data.toCharArray(dataArray, 30);
+//
+//        // // 使用sscanf解析数据
+          sscanf(dataArray, "%d %d %d %d %d %d", &speed1, &speed2, &speed3, &speed4, &position_x, &position_y);
+        // // 打印解析后的数据
+          Serial.print("speed1: ");
+          Serial.print(speed1);
+          Serial.print(" speed2: ");
+          Serial.print(speed2);
+          Serial.print(" speed3: ");
+          Serial.print(speed3);
+          Serial.print(" speed4: ");
+          Serial.print(speed4);
+          Serial.print(" position_x: ");
+          Serial.print(position_x);
+          Serial.print(" position_y: ");
+          Serial.print(position_y);
+          Serial.print(" Time: ");
+          Serial.print(elapsedTime);
+          Serial.print("\n");
+          data = "";
+          stringComplete = false;
+        
 
-        Serial.print(data);
+        //Serial.print(data);
 
         endTime = millis();
         elapsedTime = endTime - startTime;
 
-        Serial.print(" Time: ");
-        Serial.print(elapsedTime);
-        Serial.print("\n");
+        
+        }
+        //while (Serial.read() >= 0) {
+            // Clear the buffer
+        //}
 
-        // data.toCharArray(dataArray, 30);
+        
+    
+}
 
-        // // 使用sscanf解析数据
-        // sscanf(dataArray, "%d %d %d %d %d %d", &speed1, &speed2, &speed3, &speed4, &position_x, &position_y);
-        // // 打印解析后的数据
-        // Serial.print("speed1: ");
-        // Serial.print(speed1);
-        // Serial.print(" speed2: ");
-        // Serial.print(speed2);
-        // Serial.print(" speed3: ");
-        // Serial.print(speed3);
-        // Serial.print(" speed4: ");
-        // Serial.print(speed4);
-        // Serial.print(" position_x: ");
-        // Serial.print(position_x);
-        // Serial.print(" position_y: ");
-        // Serial.print(position_y);
-        // Serial.print(" Time: ");
-        // Serial.print(elapsedTime);
-        // Serial.print("\n");
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    data += inChar;
+    if (inChar == '\n') {
+      stringComplete = true;
     }
+  }
 }
