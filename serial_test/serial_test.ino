04f889a9bc7fc0ca13bc5b79e6866a54aaeb638b
张锦晨 <zjcjim@outlook.com>
2024-07-13 14:17:17 +0800

    Improve serial communication in serial_test.ino

--- serial_test/serial_test.ino
+++ serial_test/serial_test.ino
@@ -8,9 +8,7 @@ void loop() {
     if (Serial.available()) {
         // 读取一行数据，直到换行符'\n'
         String data = Serial.readStringUntil('\n');
-        data = data.substring(0, data.length() - 1);  // 去掉换行符'\n'
         // 打印读取到的数据
         Serial.println("Received: " + data);
-        Serial.read();  // 读取换行符'\n'
     }
 }
