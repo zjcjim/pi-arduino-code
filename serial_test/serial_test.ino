2f80831b247d8bc7a4717deda03b26b30eeeda03
张锦晨 <zjcjim@outlook.com>
2024-07-13 15:01:57 +0800

    feat: Improve serial communication parsing in serial_test.ino

--- serial_test/serial_test.ino
+++ serial_test/serial_test.ino
@@ -13,38 +13,24 @@ void loop() {
         String data = Serial.readStringUntil('\n');
         // 打印读取到的数据
         Serial.print("Received: " + data);
-        // 提取整数
-        motor1 = data.substring(0, data.indexOf('M')).toInt();
-        data = data.substring(data.indexOf('M') + 2);  // "20MB30MC40MD0.1SA0.2SB"
+        // 解析数据
+        char dataArray[30];
+        data.toCharArray(dataArray, 30);
 
-        motor2 = data.substring(0, data.indexOf('M')).toInt();
-        data = data.substring(data.indexOf('M') + 2);  // "30MC40MD0.1SA0.2SB"
-
-        motor3 = data.substring(0, data.indexOf('M')).toInt();
-        data = data.substring(data.indexOf('M') + 2);  // "40MD0.1SA0.2SB"
-
-        motor4 = data.substring(0, data.indexOf('M')).toInt();
-        data = data.substring(data.indexOf('M') + 2);  // "0.1SA0.2SB"
-
-        // 提取浮点数
-        position_x = data.substring(0, data.indexOf('S')).toFloat();
-        data = data.substring(data.indexOf('S') + 2);  // "0.2SB"
-
-        position_y = data.substring(0, data.indexOf('S')).toFloat();
-
-        // 打印结果
+        // 使用sscanf解析数据
+        sscanf(dataArray, "%dMA%dMB%dMC%dMD%fSA%fSB", &motor1, &motor2, &motor3, &motor4, &position_x, &position_y);
+        // 打印解析后的数据
         Serial.print("motor1: ");
         Serial.print(motor1);
-        Serial.print("motor2: ");
+        Serial.print(" motor2: ");
         Serial.print(motor2);
-        Serial.print("motor3: ");
+        Serial.print(" motor3: ");
         Serial.print(motor3);
-        Serial.print("motor4: ");
+        Serial.print(" motor4: ");
         Serial.print(motor4);
-
-        Serial.print("servo1: ");
-        Serial.print(position_x, 2);
-        Serial.print("servo2: ");
-        Serial.println(position_y, 2);
+        Serial.print(" position_x: ");
+        Serial.print(position_x);
+        Serial.print(" position_y: ");
+        Serial.println(position_y);
     }
 }
