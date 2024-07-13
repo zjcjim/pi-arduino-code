7ae47c8723322097a9cec4ea2f2d3bf7f0d7c113
张锦晨 <zjcjim@outlook.com>
2024-07-13 15:28:08 +0800

    feat: Improve serial communication parsing and data handling in serial_test.ino

--- serial_test/serial_test.ino
+++ serial_test/serial_test.ino
@@ -1,38 +1,35 @@
 int motor1, motor2, motor3, motor4;
 int position_x, position_y;
+char dataArray[30];
 
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(9600);
-    String data = "10 20 30 40 1 -2";
-    char dataArray[30];
-    data.toCharArray(dataArray, 30);
-
-    // 使用sscanf解析数据
-    sscanf(dataArray, "%d %d %d %d %d %d", &motor1, &motor2, &motor3, &motor4, &position_x, &position_y);
-    // 打印解析后的数据
-    Serial.print("motor1: ");
-    Serial.print(motor1);
-    Serial.print(" motor2: ");
-    Serial.print(motor2);
-    Serial.print(" motor3: ");
-    Serial.print(motor3);
-    Serial.print(" motor4: ");
-    Serial.print(motor4);
-    Serial.print(" position_x: ");
-    Serial.print(position_x);
-    Serial.print(" position_y: ");
-    Serial.println(position_y);
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
-    // if (Serial.available()) {
-    //     // 读取一行数据，直到换行符'\n'
-    //     String data = Serial.readStringUntil('\n');
-    //     // 打印读取到的数据
-    //     Serial.print("Received: " + data);
-    //     // 解析数据
+    if (Serial.available()) {
+        // 读取一行数据，直到换行符'\n'
+        String data = Serial.readStringUntil('\n');
 
-    // }
+        data.toCharArray(dataArray, 30);
+
+        // 使用sscanf解析数据
+        sscanf(dataArray, "%d %d %d %d %d %d", &motor1, &motor2, &motor3, &motor4, &position_x, &position_y);
+        // 打印解析后的数据
+        Serial.print("motor1: ");
+        Serial.print(motor1);
+        Serial.print(" motor2: ");
+        Serial.print(motor2);
+        Serial.print(" motor3: ");
+        Serial.print(motor3);
+        Serial.print(" motor4: ");
+        Serial.print(motor4);
+        Serial.print(" position_x: ");
+        Serial.print(position_x);
+        Serial.print(" position_y: ");
+        Serial.println(position_y);
+        // 解析数据
+    }
 }
